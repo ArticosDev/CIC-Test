f92b6071771105d85bba03fd42c64a326a7b33aa
Articos <work.Alfredo.inostroza@gmail.com>
2021-05-21 08:01:06 -0400

    Added Initial Front for login

--- utilities/jwt.js
+++ utilities/jwt.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
-const generarJWT = (uid, name) => {
+const generarJWT = (uid) => {
 
     //Payload for sing
-    const payload = { uid, name };
+    const payload = { uid };
 
 
     return new Promise((resolve, reject) => {
